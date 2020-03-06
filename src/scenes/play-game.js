5faf9ebf542df4583d0f3cbda73a3c934935903c
dizzydogg <jeffthetenor@yahoo.com>
2020-03-06 12:45:05 -0700

    create keyboard handling

--- src/scenes/play-game.js
+++ src/scenes/play-game.js
@@ -1,4 +1,5 @@
 import SocketSession from '../networking/socket-session';
+import KeyBoardController from '../controllers/keyboard';
 
 export default class PlayGameScene extends Phaser.Scene {
     constructor (config, key = 'PlayGame') {
@@ -17,45 +18,6 @@ export default class PlayGameScene extends Phaser.Scene {
 
     preload () {
         // load all the resources required for this scene before using them
-        // Creates object for input with WASD kets
-//        moveKeys = this.input.keyboard.addKeys({
-//            'up': Phaser.Input.Keyboard.KeyCodes.W,
-//            'down': Phaser.Input.Keyboard.KeyCodes.S,
-//            'left': Phaser.Input.Keyboard.KeyCodes.A,
-//            'right': Phaser.Input.Keyboard.KeyCodes.D
-//        });
-//
-//        // Enables movement of player with WASD keys
-//        this.input.keyboard.on('keydown_W', function (event) {
-//            player.setAccelerationY(-800);
-//        });
-//        this.input.keyboard.on('keydown_S', function (event) {
-//            player.setAccelerationY(800);
-//        });
-//        this.input.keyboard.on('keydown_A', function (event) {
-//            player.setAccelerationX(-800);
-//        });
-//        this.input.keyboard.on('keydown_D', function (event) {
-//            player.setAccelerationX(800);
-//        });
-//
-//        // Stops player acceleration on uppress of WASD keys
-//        this.input.keyboard.on('keyup_W', function (event) {
-//            if (moveKeys['down'].isUp)
-//                player.setAccelerationY(0);
-//        });
-//        this.input.keyboard.on('keyup_S', function (event) {
-//            if (moveKeys['up'].isUp)
-//                player.setAccelerationY(0);
-//        });
-//        this.input.keyboard.on('keyup_A', function (event) {
-//            if (moveKeys['right'].isUp)
-//                player.setAccelerationX(0);
-//        });
-//        this.input.keyboard.on('keyup_D', function (event) {
-//            if (moveKeys['left'].isUp)
-//                player.setAccelerationX(0);
-//        });
     }
 
     create () {
@@ -63,6 +25,11 @@ export default class PlayGameScene extends Phaser.Scene {
         this.socketSession = new SocketSession(this);
 
         this.socketSession.connect();
+
+        this.physics.world.setBoundsCollision(true, true, true, true);
+        this.createPlayer();
+        this.createControllers();
+        this.keyboardsniffer = new KeyBoardController(this);
     }
 
     update () {
@@ -80,5 +47,11 @@ export default class PlayGameScene extends Phaser.Scene {
             this.game.appMessage.value = '';
             // Parse contents for commands
         }
+    }
+
+    createPlayer () {
+    }
+
+    createControllers () {
     }
 };
