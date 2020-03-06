f5bf52ab17aba4a7ea7c1909d3466e1e67b0a0f4
HookBot <hookbot@r.cx>
2020-03-06 16:34:24 -0700

    Fix barrell aim to match true mouse target

--- src/controllers/barrel.js
+++ src/controllers/barrel.js
@@ -1,7 +1,7 @@
 export default class Barrel {
     constructor (scene, barrel) {
         scene.input.on('pointermove', function (pointer) {
-            barrel.rotation = Phaser.Math.Angle.Between(barrel.x, barrel.y, pointer.x, pointer.y) + (3.14159/2);
+            barrel.rotation = Phaser.Math.Angle.Between(barrel.x, barrel.y, pointer.x + scene.cameras.main.scrollX, pointer.y + scene.cameras.main.scrollY) + (Math.PI/2);
         }, this);
     }
 }
